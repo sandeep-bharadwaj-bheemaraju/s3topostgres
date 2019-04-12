5dff7ce0e2f0a1c2a82cd4f667c123105d041068
sandeep-java-addict <sandeep.java.addict@gmail.com>
2019-04-12 10:22:26 +1000

    initial commit

---
+++ src/main/java/com/sandeep/learning/s3topostgres/S3topostgresApplication.java
@@ -0,0 +1,13 @@
+package com.sandeep.learning.s3topostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3topostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3topostgresApplication.class, args);
+	}
+
+}
